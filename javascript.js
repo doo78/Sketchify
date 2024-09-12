ad8e9646107f2c9b6b09920e92b20baec982613c
doo78 <rickydeangordon@gmail.com>
2024-09-12 14:46:05 +0100

    Finished with the basics of sketching and resizing the canvas by either 16px 32px or 64px. I concluded that values inbetween wouldn't be that relevant to the user. Next I'll implement a colour palette.

--- javascript.js
+++ javascript.js
@@ -2,13 +2,15 @@
 const container = document.querySelector(".container");
 let isMouseDown = false;
 
+// Checks if the user is holding the mouse down
 document.body.onmousedown = (e) => {
     isMouseDown = true;
-    e.preventDefault();
+    e.preventDefault(); // Prevents an error when dragging an already black square
 }
 
+// Checks if the user has released the mouse
 document.addEventListener('mouseup', function() {
-    isMouseDown = false;  // Set the flag to true when mouse is down
+    isMouseDown = false;  
 });
 
 function createGrid(size){
@@ -21,9 +23,11 @@ function createGrid(size){
         const boxRow = document.createElement("div");
         boxRow.setAttribute("id", "boxRow");
         
+        // Calculates the dimensions of each box based on the height of the container and how many pixels
         const containerHeight = container.offsetHeight;
         let heightWidth = Math.floor((parseInt(containerHeight)/size));
 
+        // Adds a box to the row
         for (let i = 0; i < size; i++){
             
             const boxToAdd = document.createElement("div");
@@ -39,9 +43,8 @@ function createGrid(size){
         return boxRow;
     }
 
-
+    // Creates a row for as many columns there are
     for (let i = 0; i < size; i++){
-        
         
         container.appendChild(createRow());
     }
@@ -54,69 +57,61 @@ function createGrid(size){
 
 createGrid(32);
 
+function generateRandomColour(){
+    let value1 = Math.random()*255;
+    let value2 = Math.random()*255;
+    let value3 = Math.random()*255;
+    
+    let randomColour = ("rgb(" + value1 + "," + value2 + "," + value3 + ")")
+    
+    return randomColour;
+}
+
+// Adds event listeners to the boxes
 function initalizeBox(){
+
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
+
         box.addEventListener("mouseenter", () => {
 
+            // Adds a border to the box the cursor hovers over
             box.style.border = "0.5px solid black";
 
+            // Changes the colour if the user is holding the mouse down
             if (isMouseDown){
-            
-            /*
-            let value1 = Math.random()*255;
-            let value2 = Math.random()*255;
-            let value3 = Math.random()*255;
-            */
-            
-            /*
-            let randomColour = ("rgb(" + value1 + "," + value2 + "," + value3 + ")")
-            box.style.backgroundColor = randomColour;
-            
-
-
-            let currentOpacity = parseFloat(box.style.opacity);
-            if (isNaN(currentOpacity)) {
-                currentOpacity = 0; 
+                box.style.backgroundColor = "black";
             }
 
-            console.log("1" + currentOpacity);
-            box.style.opacity = currentOpacity + 0.1;
-            console.log("2" + box.style.opacity);
-            });
-            */
-            box.style.backgroundColor = "black";
-            }
-            });
+        });
 
-     
-
+        // Resets the border when the mouse leaves
         box.addEventListener("mouseleave", () => {
             box.style.border = "none";
         });
 
+        // Changes the colour of the box when it is clicked instead of dragged past
         box.addEventListener("click", () => {
             box.style.backgroundColor = "black";
         });
     });
 }
 
+// Buttons for changing the pixel size
 
-const sizeButton = document.querySelector("#size");
+const sizeBtns = document.querySelectorAll(".sizeBtn");
 
-sizeButton.addEventListener("click", () => {
-    let size = prompt("Enter the number of squares per side you want: ");
+sizeBtns.forEach((sizeBtn) => {
 
-    if (size > 100){
-        alert("The max size is 100.");
-    }
+    sizeBtn.addEventListener("click", () => {
 
-    else{
         while (container.hasChildNodes()){
             container.removeChild(container.lastChild);
         }
 
-        
+        let size = sizeBtn.textContent.slice(0,2);
+
         createGrid(size);
-    }
+        
+    });
 });
