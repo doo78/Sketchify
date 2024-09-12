d4c78ce31828b66f529204c1487e46467277cd6b
doo78 <rickydeangordon@gmail.com>
2024-09-12 13:55:26 +0100

    Had an issue where the cursor was changing to an error when I tried dragging on an already black square. I found someone with the same problem on stack overflow and implemented the same solution to fix it.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 
 const container = document.querySelector(".container");
+let isMouseDown = false;
+
+document.body.onmousedown = (e) => {
+    isMouseDown = true;
+    e.preventDefault();
+}
+
+document.addEventListener('mouseup', function() {
+    isMouseDown = false;  // Set the flag to true when mouse is down
+});
 
 function createGrid(size){
 
@@ -15,6 +25,7 @@ function createGrid(size){
         let heightWidth = Math.floor((parseInt(containerHeight)/size));
 
         for (let i = 0; i < size; i++){
+            
             const boxToAdd = document.createElement("div");
             boxToAdd.classList.add("box")
 
@@ -31,6 +42,7 @@ function createGrid(size){
 
     for (let i = 0; i < size; i++){
         
+        
         container.appendChild(createRow());
     }
 
@@ -40,38 +52,51 @@ function createGrid(size){
 
 
 
-createGrid(16);
+createGrid(32);
 
 function initalizeBox(){
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((box) => {
-        box.addEventListener("mouseover", () => {
+        box.addEventListener("mouseenter", () => {
 
-            console.log("KILL MEE!!!")
-        
-        /*
-        let value1 = Math.random()*255;
-        let value2 = Math.random()*255;
-        let value3 = Math.random()*255;
-        */
-        
-        /*
-        let randomColour = ("rgb(" + value1 + "," + value2 + "," + value3 + ")")
-        box.style.backgroundColor = randomColour;
-        
+            box.style.border = "0.5px solid black";
+
+            if (isMouseDown){
+            
+            /*
+            let value1 = Math.random()*255;
+            let value2 = Math.random()*255;
+            let value3 = Math.random()*255;
+            */
+            
+            /*
+            let randomColour = ("rgb(" + value1 + "," + value2 + "," + value3 + ")")
+            box.style.backgroundColor = randomColour;
+            
 
 
-        let currentOpacity = parseFloat(box.style.opacity);
-        if (isNaN(currentOpacity)) {
-            currentOpacity = 0; 
-        }
+            let currentOpacity = parseFloat(box.style.opacity);
+            if (isNaN(currentOpacity)) {
+                currentOpacity = 0; 
+            }
 
-        console.log("1" + currentOpacity);
-        box.style.opacity = currentOpacity + 0.1;
-        console.log("2" + box.style.opacity);
+            console.log("1" + currentOpacity);
+            box.style.opacity = currentOpacity + 0.1;
+            console.log("2" + box.style.opacity);
+            });
+            */
+            box.style.backgroundColor = "black";
+            }
+            });
+
+     
+
+        box.addEventListener("mouseleave", () => {
+            box.style.border = "none";
         });
-        */
-        box.style.backgroundColor = "black";
+
+        box.addEventListener("click", () => {
+            box.style.backgroundColor = "black";
         });
     });
 }
